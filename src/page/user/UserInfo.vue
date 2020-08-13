b9774b55c9e82c5566b7cfadecce999d07c108e0
dtwangjiliang <845506513@qq.com>
2020-08-13 14:42:34 +0800

    初始化

---
+++ src/page/user/UserInfo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>msg : {{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserInfo',
+  data () {
+    return {
+      msg: 'Welcome Liang'
+    }
+  }
+}
+</script>
